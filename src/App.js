cacc993b8e518b9a2ea21a26756bbd3fb6e6a72b
Valery <ilchenko.valery.developer@gmail.com>
2023-12-26 19:31:24 +0200

    added styles

--- src/App.js
+++ src/App.js
@@ -8,18 +8,19 @@ function App() {
   const [missDistance, setMissDistance] = useState(10);
   const [result, setResult] = useState();
 
-  const scopeClickCount = () => {
-
+  const scopeClickCount = (e) => {
+    e.preventDefault();
+  
     const oneOneOnOneHUndredMeters = 2.908 * targetDistance / 100
     const correctionDistanceInMoa = missDistance / oneOneOnOneHUndredMeters
     const clicksPerOneMoa = () => {
       switch (moa) {
         case "1/8":
-          return 1 / 8 ** -1
+          return 8
         case "1/4":
-          return 1 / 4 ** -1
+          return 4
         case "1/2":
-          return 1 / 2 ** -1
+          return 2
         default:
           return 1
       }
@@ -27,15 +28,15 @@ function App() {
 
     setResult(Math.round(correctionDistanceInMoa * clicksPerOneMoa()));
   }
-  
+
   return (
     <div className='mainWrapper'>
       <header>
-        <h1>sight-calibration</h1>
+        <h1>Sight Calibration</h1>
       </header>
-      <div className='calculateSection'>
+      <form className='calculateSection' onSubmit={(e) => scopeClickCount(e)}>
 
-        <div>
+        <div className='moaSelector'>
           <label htmlFor="pet-select">Choose MOA:</label>
 
           <select name="pets" id="moa" onChange={(e) => setMoa(e.target.value)}>
@@ -46,19 +47,21 @@ function App() {
           </select>
         </div>
 
-        <div className="">
+        <div className="inputWrapper">
           <label htmlFor="targetDistance">Distance to target (m):</label>
           <input type="number" id="targetDistance" value={targetDistance} onChange={(e) => setTargetDistance(e.target.value)} />
         </div>
-        
-        <div className="">
+
+        <div className="inputWrapper">
           <label htmlFor="missDistance">Miss distance (cm):</label>
           <input type="number" id="missDistance" value={missDistance} onChange={(e) => setMissDistance(e.target.value)} />
         </div>
 
-        <button onClick={scopeClickCount}>Calculate</button>
-        {result}
-      </div>
+        <button type='onSubmit' className="buttonWrapper">Calculate</button>
+        <div className="resultWrapper">
+          Click count: <span className='result'>{result}</span>
+        </div>
+      </form>
     </div>
   );
 }
